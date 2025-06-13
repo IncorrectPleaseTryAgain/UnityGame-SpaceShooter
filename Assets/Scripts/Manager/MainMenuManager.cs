daa2a91d47d032e0950ea17cb2d945dc4563c5a1
Michael Steenkamp <michael.steenkamp@icloud.com>
2025-06-12 21:31:55 -0600

    Update v1.0.3.2
    *Changed Planet Icon Visibility

--- Assets/Scripts/Manager/MainMenuManager.cs
+++ Assets/Scripts/Manager/MainMenuManager.cs
@@ -8,13 +8,11 @@ public class MainMenuManager : MonoBehaviour
     [SerializeField] private TMP_Text bestRound;
     [SerializeField] private TMP_Text bestTime;
     [SerializeField] private TMP_Text newRecord;
-    [SerializeField] private GameObject earthIcon;
 
     private void Start() {
         SaveManager.instance.LoadRound(bestRound, bestTime);
         bool isNewRecord = SaveManager.instance.GetNewRecord();
         newRecord.GetComponent<NewRecordLogic>().SetVisibility(isNewRecord);
-        earthIcon.SetActive(isNewRecord);
         SaveManager.instance.SetNewRecord(false);
     }
 }
